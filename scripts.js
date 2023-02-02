1f5f2b9184c9adf01c8632cd0cdcd2eb0235fd09
Miguel Camilo <miguelcamilo48@gmail.com>
2023-02-01 21:28:23 -0500

    initial commit

--- scripts.js
+++ scripts.js
@@ -1,39 +1,38 @@
+// deployed link
+// http://127.0.0.1:5500/fetchAPI/index.html
+
 const coffeeList = document.getElementById("coffeeList");
 
 const addSingleCoffee = (coffee) => {
-    const newListItem = document.createElement('li') // creates a list item
-    const newText = document.createTextNode(coffee.title) // create an element with the title from the object
-    newListItem.appendChild(newText) // take the list item and store title from object inside of it
-    return newListItem
-}
+	const newListItem = document.createElement("div"); // creates a list item
+	const newText = document.createTextNode(coffee.title); // create an element with the title from the object
+	newListItem.appendChild(newText); // take the list item and store title from object inside of it
+	return newListItem;
+};
 
 const newCoffeeList = (listOfCoffees) => {
-    const newList = document.createElement('ul')
-    
-    // loop through the list of coffees and add each one to this list
-    listOfCoffees.forEach(coffee => {
-        const thisItem = addSingleCoffee(coffee)
-        newList.appendChild(thisItem)
-    })
-    coffeeList.innerText = ''
-    coffeeList.appendChild(newList)
-}
+	const newList = document.createElement("ul");
+
+	// loop through the list of coffees and add each one to this list
+	listOfCoffees.forEach((coffee) => {
+		const thisItem = addSingleCoffee(coffee);
+		newList.appendChild(thisItem);
+	});
+	coffeeList.innerText = ""; // removes the spinning icon
+	coffeeList.appendChild(newList);
+};
 
 // func to fetch api data
 const getCoffee = (type) => {
-	coffeeList.innerText = "Loading...";
+	coffeeList.innerHTML = '<i class="fas fa-spinner fa-spin"></i>';
     
-	fetch(`https://api.sampleapis.com/coffee/${type}`)
-		.then((res) => res.json()) // returns a promise and .json() just grabs the body
-		.then((data) => {
-			// list data on screen
-            newCoffeeList(data)
-		})
-		.catch((err) => console.err(err));
+	setTimeout(() => {
+		fetch(`https://api.sampleapis.com/coffee/${type}`)
+			.then((res) => res.json()) // returns a promise and .json() just grabs the body
+			.then((data) => {
+				// list data on screen
+				newCoffeeList(data);
+			})
+			.catch((err) => console.log(err));
+	}, 1000);
 };
-
-
-
-//! you can use this to delay the response
-// setTimeout(() => {
-// }, 1000);
